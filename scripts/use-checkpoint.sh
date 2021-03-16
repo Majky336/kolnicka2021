b1bf07f2da7a752c02432c2c21a8da13acdb9cf0
Sojka Michal <sojka@cngroup.dk>
2021-03-16 17:36:06 +0100

    Add checking of branch status

--- scripts/use-checkpoint.sh
+++ scripts/use-checkpoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
 checkpoint=$1;
+control_branch='control'
+branch_status=null
 
-echo $checkpoint
+if [[ $(git diff --stat) != '' ]]; then
+  branch_status='dirty'
+else
+  branch_status='clean'
+fi
+
+echo $branch_status
+
+# git diff --quiet
+# git checkout $control_branch
