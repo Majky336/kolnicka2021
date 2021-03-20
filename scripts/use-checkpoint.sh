836c57b278772c62163a6fd2f834dc35e9b2d364
Michal Sojka <sojka.michal.96@gmail.com>
2021-03-20 10:51:19 +0100

    Control (#1)
    * Add checking of branch status
    
    * Add retrieving of target SHA
    
    * Add checkout to temporal branch
    
    * Add checking out to temp branch if it exists
    
    * Add cherry pick of target commit
    
    * Add cherry picking in range from first commit
    
    Co-authored-by: Sojka Michal <sojka@cngroup.dk>

--- scripts/use-checkpoint.sh
+++ scripts/use-checkpoint.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
 
 checkpoint=$1;
+control_branch='control'
+branch_status=null
+search_term="Checkpoint ${checkpoint}"
 
-echo $checkpoint
+if test "x$control_branch" = x; then
+    control_branch=`git branch -a | grep "*" | cut -d ' ' -f2`
+fi
+
+if [[ $(git diff --stat) != '' ]]; then
+  branch_status='dirty'
+  echo "Branch dirty. Resetting changes and creating temp branch"
+  git checkout .
+  echo "Cleaned git tree"  
+else
+  branch_status='clean'
+  echo "Branch clean."
+fi
+
+temp_branch=`git branch -a | grep "temp"`
+if test "y$temp_branch" = y; then
+    git checkout -b "temp"
+    echo "Set up temporal branch complete"
+else
+    git checkout "temp"
+    echo "Temporal branch already set up. Switching"
+fi
+
+first_SHA=`git log --abbrev-commit --pretty=oneline --reverse | head -1 | cut -d ' ' -f1`
+target_SHA=`git log --abbrev-commit --pretty=oneline --grep="${search_term}" | cut -d ' ' -f1` 
+
+echo $target_SHA
+
+git cherry-pick $first_SHA^..$target_SHA
