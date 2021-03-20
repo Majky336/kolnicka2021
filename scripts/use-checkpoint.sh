e8326755ee76e51755855b01179f2fec0f4a35a1
Majky336 <sojka.michal.96@gmail.com>
2021-03-20 10:41:39 +0100

    Add cherry pick of target commit

--- scripts/use-checkpoint.sh
+++ scripts/use-checkpoint.sh
@@ -13,21 +13,23 @@ if [[ $(git diff --stat) != '' ]]; then
   branch_status='dirty'
   echo "Branch dirty. Resetting changes and creating temp branch"
   git checkout .
-  echo "Cleaned git tree"
-  temp_branch=`git branch -a | grep "temp"`
-  if test "y$temp_branch" = y; then
-    git checkout -b "temp"
-    echo "Set up temporal branch complete"
-  else
-    git checkout "temp"
-    echo "Temporal branch already set up. Switching"
-  fi
-  
+  echo "Cleaned git tree"  
 else
   branch_status='clean'
   echo "Branch clean."
 fi
 
+temp_branch=`git branch -a | grep "temp"`
+if test "y$temp_branch" = y; then
+    git checkout -b "temp"
+    echo "Set up temporal branch complete"
+else
+    git checkout "temp"
+    echo "Temporal branch already set up. Switching"
+fi
+
 target_SHA=`git log --abbrev-commit --pretty=oneline --grep="${search_term}" | cut -d ' ' -f1` 
 
 echo $target_SHA
+
+git cherry-pick $target_SHA
